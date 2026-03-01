5fb149a978214e0609a10a03e10b4f81dc70c17c
adberrywb-art <adberrywb@gmail.com>
2026-02-28 21:35:38 -0600

    Change domain and price ID in server.rb
    Updated the domain and price ID for the checkout session.

--- server.rb
+++ server.rb
@@ -7,7 +7,7 @@ Stripe.api_key = 'sk_test_51SgaUKRvcXwFOa1F0viO5izv6MM4ccDWuAZ5ZC1nPXEDfDU4e6Fxx
 set :static, true
 set :port, 4242
 
-YOUR_DOMAIN = 'http://localhost:4242'
+https://www.tradingprovipsignals.online/ = 'http://localhost:4242'
 
 post '/create-checkout-session' do
   content_type 'application/json'
@@ -15,7 +15,7 @@ post '/create-checkout-session' do
   session = Stripe::Checkout::Session.create({
     line_items: [{
       # Provide the exact Price ID (for example, price_1234) of the product you want to sell
-      price: '{{PRICE_ID}}',
+      price: '{{price_1T5GIMRvcXwFOa1Fh8Sj9PjD}}',
       quantity: 1,
     }],
     mode: 'payment',
